f4b50f895bdda5d7574c4dbdd5e56854d4b94d45
ali ghodsi <alighodsi381>
2025-11-22 20:34:02 +0330

    Prepare for Vercel deployment

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,50 +1,38 @@
 import axios from "axios";
 
 
+
 const control = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+});
 
-                baseURL :"http://localhost:9000"
-})
 
-export async function Getproduct (){
-
-                const {data} = await control.get ("/articles")
-                return data
+export async function Getproduct() {
+    const { data } = await control.get("/articles");
+    return data;
 }
 
-export async function Getcartpage (id : number|string){
-
-                const{data} = await control.get(`/articles/${id}`)
-
-                return data
-
-
+export async function Getcartpage(id: number | string) {
+    const { data } = await control.get(`/articles/${id}`);
+    return data;
 }
 
-export async function Gethistory (){
-
-                const {data} = await control.get("/history")
-                return data
-}
-export async function Gethistorypage (id : number|string){
-
-                const{data} = await control.get(`/history/${id}`)
-
-                return data
-
-
-}
-export async function Getnaval(){
-
-                const {data} = await control.get("/naval")
-                return data
+export async function Gethistory() {
+    const { data } = await control.get("/history");
+    return data;
 }
 
-export async function Getnavalpage (id : number|string){
+export async function Gethistorypage(id: number | string) {
+    const { data } = await control.get(`/history/${id}`);
+    return data;
+}
 
-                const{data} = await control.get(`/naval/${id}`)
+export async function Getnaval() {
+    const { data } = await control.get("/naval");
+    return data;
+}
 
-                return data
-
-
+export async function Getnavalpage(id: number | string) {
+    const { data } = await control.get(`/naval/${id}`);
+    return data;
 }
